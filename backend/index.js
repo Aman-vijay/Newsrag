4ac56ada841e1c6ac94282b13a97a0533a71895b
Aman-vijay <shivamvj04@gmail.com>
2025-09-26 23:28:52 +0530

    feat: Add welcome route to NewsRAG API and remove unused app.scss file

--- backend/index.js
+++ backend/index.js
@@ -29,6 +29,10 @@ app.use(cors(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the NewsRAG API');
+});
+
 app.get('/health', (req, res) => {
     res.send('Server is healthy');
 });
